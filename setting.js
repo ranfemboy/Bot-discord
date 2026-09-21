7968e1608e33164580422a300c2f26b3487c8a48
ran - Femboy <ranzxx62@gmail.com>
2026-09-21 09:29:00 +0700

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,38 @@
+export default {
+    token: 'YOUR_TOKEN_BOT',
+
+    // Prefix command, contoh: .menu / !menu / #menu
+    prefix: '.',
+
+    // ID Discord owner bot (klik kanan profil -> Copy User ID, aktifkan Developer Mode dulu)
+    idOwner: 'YOUR_ID',
+
+    // Nama owner yang ditampilkan di menu
+    ownerName: 'YOUR_NAME',
+
+    botName: 'bot discord',
+    footerText: '© bot discord',
+
+    thumbnailUrl: 'https://x.xcute.workers.dev/f/images/da6459dbf20f.jpg',
+
+    menuSound: 'https://files.catbox.moe/b3rysx.mp3', // muncul sebagai audio player di pesan menu
+
+    menuBanners: [
+        'https://x.xcute.workers.dev/f/images/80c3a8491f2e.jpg',
+        'https://x.xcute.workers.dev/f/images/a744d1a4da79.jpg',
+        'https://x.xcute.workers.dev/f/images/46e30dae9c3d.jpg',
+    ],
+
+    supportServerUrl: 'https://discord.gg/cSGfUadrHa',
+    supportEmoji: '<a:zclyde_snow:1474998418446483668>',
+
+// ====== TELEGRAM BACKUP ======
+    telegramBotToken: 'BOT_TELEGRAM_TOKEN',
+    telegramChatId: 'YOUR ID TELEGRAM', // ID user/grup tujuan backup
+
+    saweriaLink: 'https://saweria.co/ranzzchan',
+    saweriaChannelId: '',
+    // ⚠️ Secret buat verifikasi webhook, jangan sampai bocor
+    saweriaSecret: '18609b7f5fc1d4ce1c7f8f93fc2b5298',
+    saweriaMessage: '🎉 **{nama}** baru aja donasi **Rp{jumlah}**!\n💬 "{pesan}"',
+};
